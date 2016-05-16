e777ed2dfc8e14e094c91980d665fea2228c3575
Benjamin Holmes <benjamin.holmes@gmail.com>
2016-05-16 21:02:25 +0100

    Initial commit

---
+++ src/main/java/com/redhat/bh/GreetingRoute.java
@@ -0,0 +1,55 @@
+/**
+ *  Copyright 2005-2015 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package com.redhat.bh;
+
+import javax.inject.Inject;
+
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.Endpoint;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cdi.ContextName;
+import org.apache.camel.cdi.Uri;
+
+import io.fabric8.annotations.Alias;
+import io.fabric8.annotations.ServiceName;
+
+/**
+ * Configures all our Camel routes, components, endpoints and beans
+ */
+@ContextName("camelContext")
+public class GreetingRoute extends RouteBuilder {
+
+	@Inject
+	@Uri("cxfrs:http://localhost:9102?resourceClasses=com.redhat.bh.cxf.GreetingService&bindingStyle=SimpleConsumer")
+	private Endpoint cxfEndpoint;
+
+	@Inject
+	@ServiceName("broker-amq-tcp")
+	@Alias("amq")
+	ActiveMQComponent activeMQComponent;
+
+	@Inject
+	@Uri("log:output")
+	private Endpoint resultEndpoint;
+
+	@Override
+	public void configure() throws Exception {
+		// you can configure the route rule with Java DSL here
+
+		from(cxfEndpoint).to("amq:queue:greetings");
+	}
+
+}
---
+++ src/main/java/com/redhat/bh/cxf/GreetingService.java
@@ -0,0 +1,17 @@
+package com.redhat.bh.cxf;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path(value = "/greeting")
+public interface GreetingService {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{name}")
+	String greeting(@PathParam("name") String name);
+
+}
