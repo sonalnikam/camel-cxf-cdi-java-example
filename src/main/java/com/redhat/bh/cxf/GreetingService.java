9293b17375ba73573938dfab298b97d7bdcd6501
Benjamin Holmes <benjamin.holmes@gmail.com>
2016-05-17 10:32:32 +0100

    Updated greeting service to be more user friendly

--- src/main/java/com/redhat/bh/cxf/GreetingService.java
+++ src/main/java/com/redhat/bh/cxf/GreetingService.java
@@ -2,16 +2,16 @@ package com.redhat.bh.cxf;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path(value = "/greeting")
+@Path(value = "/")
 public interface GreetingService {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/{name}")
-	String greeting(@PathParam("name") String name);
+	@Path("/greeting")
+	String greeting(@QueryParam("name") String name);
 
 }
